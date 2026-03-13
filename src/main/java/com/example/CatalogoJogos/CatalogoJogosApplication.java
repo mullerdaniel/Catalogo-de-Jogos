5287b498029e02dd798796d5f597cd4eab0e85b0
Daniel Müller <miladaniel370@gmail.com>
2026-03-13 14:02:39 -0300

    Feat: Estruturação base springBoot

---
+++ src/main/java/com/example/CatalogoJogos/CatalogoJogosApplication.java
@@ -0,0 +1,13 @@
+package com.example.CatalogoJogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CatalogoJogos/CatalogoJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CatalogoJogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
